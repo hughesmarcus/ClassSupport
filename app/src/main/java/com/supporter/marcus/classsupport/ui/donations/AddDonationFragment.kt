6e125acc50ab443347da7c887b4ff477885bf21c
Marcus Hughes <marcushughes80@gmail.com>
2018-06-22 16:09:27 -0400

    Add donation button

---
+++ app/src/main/java/com/supporter/marcus/classsupport/ui/donations/AddDonationFragment.kt
@@ -0,0 +1,26 @@
+package com.supporter.marcus.classsupport.ui.donations
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.supporter.marcus.classsupport.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class AddDonationFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_donation, container, false)
+    }
+
+
+}
